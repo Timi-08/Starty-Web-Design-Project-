ba76f215629dd888d7caa30c6e03dec2f68c42df
Austin <austindike0818@gmail.com>
2025-01-07 08:51:05 -1100

    Austin-Dike-About-Page

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".slides").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrow: false,
+});
+
+$(".slider-track").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrow: false,
+});
